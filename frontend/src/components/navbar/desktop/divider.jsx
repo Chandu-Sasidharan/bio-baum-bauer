c7f825e37f5983f7f532c312cef393e347824573
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-01-04 23:26:03 +0100

    add a divider between desktop navbar menu-links

---
+++ frontend/src/components/navbar/desktop/divider.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Divider() {
+  return <div className='bg-accent w-[1px] flex-grow'></div>;
+}
--- frontend/src/components/navbar/desktop/index.jsx
+++ frontend/src/components/navbar/desktop/index.jsx
@@ -1,15 +1,11 @@
 import { useEffect } from 'react';
-import { FcFaq } from 'react-icons/fc';
+import { RiQuestionAnswerFill } from 'react-icons/ri';
+import { RiFileInfoFill } from 'react-icons/ri';
 import { MdContacts } from 'react-icons/md';
-import {
-  FaHome,
-  FaTree,
-  FaRegNewspaper,
-  FaInfoCircle,
-  FaImages,
-} from 'react-icons/fa';
+import { FaHome, FaTree, FaRegNewspaper, FaImages } from 'react-icons/fa';
 import AuthNavbar from '@/components/navbar/auth';
 import NavMenuLink from '@/components/navbar/desktop/nav-menu-link';
+import Divider from './divider';
 
 export default function DesktopNavbar({ isNavbarFixed, setIsNavbarFixed }) {
   /* Handle DesktopNavbar on Scroll */
@@ -37,29 +33,39 @@ export default function DesktopNavbar({ isNavbarFixed, setIsNavbarFixed }) {
         style={desktopNavbarStyle}
       >
         {/* DesktopNavbar Links */}
-        <div className='flex gap-10 lg:gap-20 xl:gap-24'>
+        <div className='flex gap-4 lg:gap-9 xl:gap-12 2xl:gap-16'>
           <NavMenuLink to='/' pageName='Home' icon={<FaHome />}></NavMenuLink>
+          <Divider />
           <NavMenuLink
             to='/trees'
             pageName='Trees'
             icon={<FaTree />}
           ></NavMenuLink>
+          <Divider />
           <NavMenuLink
             to='/news'
             pageName='News'
             icon={<FaRegNewspaper />}
           ></NavMenuLink>
+          <Divider />
           <NavMenuLink
             to='/about'
             pageName='About'
-            icon={<FaInfoCircle />}
+            icon={<RiFileInfoFill />}
           ></NavMenuLink>
+          <Divider />
           <NavMenuLink
             to='/gallery'
             pageName='Gallery'
             icon={<FaImages />}
           ></NavMenuLink>
-          <NavMenuLink to='/faq' pageName='FAQs' icon={<FcFaq />}></NavMenuLink>
+          <Divider />
+          <NavMenuLink
+            to='/faq'
+            pageName='FAQs'
+            icon={<RiQuestionAnswerFill />}
+          ></NavMenuLink>
+          <Divider />
           <NavMenuLink
             to='/contact'
             pageName='Contact'
