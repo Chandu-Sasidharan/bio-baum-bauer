74250b92e4b9c380462a2d2b679e1955014e25ea
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-01-04 16:39:49 +0100

    extract desktop navbar menu link to a new component and improve styles

--- frontend/src/components/navbar/desktop/index.jsx
+++ frontend/src/components/navbar/desktop/index.jsx
@@ -1,6 +1,4 @@
 import { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import AuthNavbar from '@/components/navbar/auth';
 import { FcFaq } from 'react-icons/fc';
 import { MdContacts } from 'react-icons/md';
 import {
@@ -10,6 +8,8 @@ import {
   FaInfoCircle,
   FaImages,
 } from 'react-icons/fa';
+import AuthNavbar from '@/components/navbar/auth';
+import NavMenuLink from '@/components/navbar/desktop/nav-menu-link';
 
 export default function DesktopNavbar({ isNavbarFixed, setIsNavbarFixed }) {
   /* Handle DesktopNavbar on Scroll */
@@ -33,144 +33,38 @@ export default function DesktopNavbar({ isNavbarFixed, setIsNavbarFixed }) {
     <>
       <AuthNavbar />
       <nav
-        className='desktop-nav-z-index-v-10 text-accent bg-primary top-0 flex h-[60px] w-full items-center justify-center'
+        className='desktop-nav-z-index-v-10 text-accent bg-primary items-ce top-0 flex h-[60px] items-center justify-center'
         style={desktopNavbarStyle}
       >
         {/* DesktopNavbar Links */}
-        <div className='bg-primary flex h-full w-full items-center justify-center'>
-          <div className='duration-400 hover:text-gray-dark flex items-center border-r p-2 transition-transform ease-linear hover:scale-110 sm:px-4 lg:px-8 xl:px-14'>
-            <NavLink
-              className='flex items-center justify-center gap-1 hover:border-b-2'
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      borderBottom: '2px solid #5a6448',
-                      color: '#5a6448',
-                      scale: '1.1',
-                    }
-                  : undefined
-              }
-              aria-label='Link to Home Page '
-              to='/'
-            >
-              <FaHome color='#5a6448' />
-              <span>Home</span>
-            </NavLink>
-          </div>
-          <div className='duration-400 hover:text-gray-darkitems-center flex border-r px-2 transition-transform ease-linear hover:scale-110 sm:px-4 lg:px-8 xl:px-14'>
-            <NavLink
-              className='flex items-center justify-center gap-1 hover:border-b-2'
-              to='/trees'
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      borderBottom: '2px solid #5a6448',
-                      color: '#5a6448',
-                      scale: '1.1',
-                    }
-                  : undefined
-              }
-              aria-label='Link to Sponsor page'
-            >
-              <FaTree color='#5a6448' />
-              <span>Trees</span>
-            </NavLink>
-          </div>
-          <div className='duration-400 hover:text-gray-dark flex items-center border-r px-2 transition-transform ease-linear hover:scale-110 sm:px-4 lg:px-8 xl:px-14'>
-            <NavLink
-              className='flex items-center justify-center gap-1 hover:border-b-2'
-              to='/news'
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      borderBottom: '2px solid #5a6448',
-                      color: '#5a6448',
-                      scale: '1.1',
-                    }
-                  : undefined
-              }
-              aria-label='News page'
-            >
-              <FaRegNewspaper color='#5a6448' />
-              <span>News</span>
-            </NavLink>
-          </div>
-          <div className='duration-400 hover:text-gray-dark flex items-center border-r px-2 transition-transform ease-linear hover:scale-110 sm:px-4 lg:px-8 xl:px-14'>
-            <NavLink
-              className='flex items-center justify-center gap-1 hover:border-b-2'
-              to='/about'
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      borderBottom: '2px solid #5a6448',
-                      color: '#5a6448',
-                      scale: '1.1',
-                    }
-                  : undefined
-              }
-              aria-label='About page'
-            >
-              <FaInfoCircle color='#5a6448' />
-              <span>About</span>
-            </NavLink>
-          </div>
-          <div className='duration-400 hover:text-gray-dark flex items-center border-r px-2 transition-transform ease-linear hover:scale-110 sm:px-4 lg:px-8 xl:px-14'>
-            <NavLink
-              className='flex items-center justify-center gap-1 hover:border-b-2'
-              to='/gallery'
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      borderBottom: '2px solid #5a6448',
-                      color: '#5a6448',
-                      scale: '1.1',
-                    }
-                  : undefined
-              }
-              aria-label='Gallery page'
-            >
-              <FaImages className='text-accent' />
-              <span>Gallery</span>
-            </NavLink>
-          </div>
-          <div className='duration-400 hover:text-gray-dark flex items-center border-r px-2 transition-transform ease-linear hover:scale-110 sm:px-4 lg:px-8 xl:px-14'>
-            <NavLink
-              className='flex items-center justify-center gap-1 hover:border-b-2'
-              to='/faq'
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      borderBottom: '2px solid #5a6448',
-                      color: '#5a6448',
-                      scale: '1.1',
-                    }
-                  : undefined
-              }
-              aria-label='FAQ page'
-            >
-              <FcFaq color='#5a6448' />
-              <span>FAQs</span>
-            </NavLink>
-          </div>
-          <div className='duration-400 hover:text-gray-dark flex items-center px-2 transition-transform ease-linear hover:scale-110 sm:px-4 lg:px-8 xl:px-14'>
-            <NavLink
-              className='flex items-center justify-center gap-1 hover:border-b-2'
-              to='/contact'
-              style={({ isActive }) =>
-                isActive
-                  ? {
-                      borderBottom: '2px solid #5a6448',
-                      color: '#5a6448',
-                      scale: '1.1',
-                    }
-                  : undefined
-              }
-              aria-label='Contact page'
-            >
-              <MdContacts color='#5a6448' />
-              <span>Contact</span>
-            </NavLink>
-          </div>
+        <div className='flex gap-10 lg:gap-20 xl:gap-24'>
+          <NavMenuLink to='/' pageName='Home' icon={<FaHome />}></NavMenuLink>
+          <NavMenuLink
+            to='/trees'
+            pageName='Trees'
+            icon={<FaTree />}
+          ></NavMenuLink>
+          <NavMenuLink
+            to='/news'
+            pageName='News'
+            icon={<FaRegNewspaper />}
+          ></NavMenuLink>
+          <NavMenuLink
+            to='/about'
+            pageName='About'
+            icon={<FaInfoCircle />}
+          ></NavMenuLink>
+          <NavMenuLink
+            to='/gallery'
+            pageName='Gallery'
+            icon={<FaImages />}
+          ></NavMenuLink>
+          <NavMenuLink to='/faq' pageName='FAQs' icon={<FcFaq />}></NavMenuLink>
+          <NavMenuLink
+            to='/contact'
+            pageName='Contact'
+            icon={<MdContacts />}
+          ></NavMenuLink>
         </div>
       </nav>
     </>
---
+++ frontend/src/components/navbar/desktop/nav-menu-link.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from 'react-router-dom';
+
+export default function NavMenuLink({ to = '/', pageName, icon }) {
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        `text-accent hover:text-golden-red hover:border-golden-red flex items-center justify-center gap-1 border-b-2 p-1 duration-300 ${
+          isActive ? 'border-accent' : 'border-primary'
+        }`
+      }
+      aria-label={`${pageName} page`}
+      to={to}
+    >
+      {icon}
+      <span>{pageName}</span>
+    </NavLink>
+  );
+}
