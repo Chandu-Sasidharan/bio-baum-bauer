66733b8c71e11d893643e62e9f6f514da6536e00
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-01-26 00:14:49 +0100

    make scroll to top visible only after scrolling 140px

--- frontend/src/components/scroll-to-top/index.jsx
+++ frontend/src/components/scroll-to-top/index.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { MdKeyboardDoubleArrowUp } from 'react-icons/md';
 
 export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 140) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='fixed right-4 bg-accent p-2 rounded-full bottom-4 shadow-lg hover:scale-110 duration-300'>
-      <a href='#'>
-        <MdKeyboardDoubleArrowUp className='text-3xl text-gray-light' />
-      </a>
-    </div>
+    isVisible && (
+      <div className='bg-accent fixed bottom-4 right-4 rounded-full p-2 shadow-lg duration-300 hover:scale-110'>
+        <a href='#'>
+          <MdKeyboardDoubleArrowUp className='text-gray-light text-3xl' />
+        </a>
+      </div>
+    )
   );
 }
