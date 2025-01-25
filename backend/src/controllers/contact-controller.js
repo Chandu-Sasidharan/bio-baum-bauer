b1f86d485b0e7a51703fea2773aba450fa9cb225
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-01-25 14:13:39 +0100

    sent contact form submission to hello mail

--- backend/src/controllers/contact-controller.js
+++ backend/src/controllers/contact-controller.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import Contact from '#src/models/contact.js';
 import formatZodError from '#src/utils/format-zod-error.js';
 import sendContactFeedback from '#src/utils/contact-feedback-email.js';
+import sendContactToTeam from '#src/utils/contact-to-team-email.js';
 
 export const createContact = async (req, res) => {
   const formData = req.body;
@@ -21,6 +22,8 @@ export const createContact = async (req, res) => {
 
     // Send feedback email
     await sendContactFeedback(formData.email, formData.message);
+    // Send contact form to team
+    await sendContactToTeam(formData.email, formData.message);
 
     return res
       .status(StatusCodes.CREATED)
---
+++ backend/src/utils/contact-to-team-email.js
@@ -0,0 +1,18 @@
+import sendEmail from '#src/utils/send-email.js';
+
+const sendContactToTeam = async (email, message) => {
+  const emailOptions = {
+    from: 'Contact Form Submission <contact-form-submission@biobaumbauer.de>',
+    replyTo: email,
+    to: 'hello@biobaumbauer.de',
+    subject: 'New Contact Form Submission',
+    templateName: 'contact-form-to-team',
+    templateData: {
+      message,
+    },
+  };
+
+  await sendEmail(emailOptions);
+};
+
+export default sendContactToTeam;
