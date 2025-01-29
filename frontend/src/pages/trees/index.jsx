3ce26f323ab63130bb11ec49ec580b1e958119b4
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-01-29 19:04:24 +0100

    reset page to 1 when category changes

--- frontend/src/pages/trees/index.jsx
+++ frontend/src/pages/trees/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Spinner from '@/components/elements/spinner';
 import Breadcrumbs from '@/components/elements/breadcrumbs';
@@ -21,6 +21,11 @@ export default function Trees() {
     page,
     limit,
   });
+
+  // Reset page when category changes
+  useEffect(() => {
+    setPage(1);
+  }, [category]);
 
   if (isLoading) {
     return <Spinner />;
--- frontend/src/pages/trees/sidebar.jsx
+++ frontend/src/pages/trees/sidebar.jsx
@@ -9,11 +9,14 @@ export default function Sidebar({ setSort, setCategory }) {
           onChange={e => setSort(e.target.value)}
           className='select border-primary w-full rounded pl-3 focus:outline-none'
         >
-          <option value=''>Sort By</option>
+          <option value='' disabled selected>
+            Sort By
+          </option>
           <option value='name:asc'>Name Ascending</option>
           <option value='name:desc'>Name Descending</option>
           <option value='price:asc'>Price Ascending</option>
           <option value='price:desc'>Price Descending</option>
+          <option value=''>All trees</option>
         </select>
       </div>
       <div className={styles.asideElement}>
@@ -22,13 +25,16 @@ export default function Sidebar({ setSort, setCategory }) {
           onChange={e => setCategory(e.target.value)}
           className='select border-primary w-full rounded pl-3 focus:outline-none'
         >
-          <option value=''>Filter By Category</option>
+          <option value='' disabled selected>
+            Filter By Category
+          </option>
           <option value='Fruit Tree'>Fruit Tree</option>
-          <option value='Nut Tree'>Nut Trees</option>
-          <option value='Flowering Tree'>Flowering Trees</option>
+          <option value='Nut Tree'>Nut Tree</option>
+          <option value='Flowering Tree'>Flowering Tree</option>
           <option value='Berry Shrubs'>Berry Shrubs</option>
           <option value='Deciduous Forest'>Deciduous Forest</option>
           <option value='Evergreen Forest'>Evergreen Forest</option>
+          <option value=''>All Categories</option>
         </select>
       </div>
     </aside>
