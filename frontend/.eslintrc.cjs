a1b5d27ff557eb8a87b990181e4cb77efdacb3b6
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-10-31 21:33:05 +0100

    modify eslint rule

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -16,5 +16,11 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_', // Ignore unused args starting with _
+      },
+    ],
   },
-}
+};
