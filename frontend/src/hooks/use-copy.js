83b22de359a277df9e5dcccdf1a7667927164305
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-11-20 20:50:52 +0100

    remove useMemo from use-copy hook

--- frontend/src/hooks/use-copy.js
+++ frontend/src/hooks/use-copy.js
@@ -1,9 +1,8 @@
-import { useMemo } from 'react';
 import { DEFAULT_LANGUAGE } from '@/constants';
 import { useLanguage } from '@/context/lang-context';
 
 export default function useCopy(copy = {}) {
   const { language } = useLanguage();
 
-  return useMemo(() => copy[language] || copy[DEFAULT_LANGUAGE] || {}, [copy, language]);
+  return copy[language] || copy[DEFAULT_LANGUAGE] || {};
 }
